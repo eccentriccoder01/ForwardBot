3ef60d0d137fc070f57f9b3e7cb079b7692b6544
Sagnik Chakraborty <sagnikgraviton847@gmail.com>
2025-03-24 11:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "28580773")
-    API_HASH = os.environ.get("API_HASH", "f80e465a8805bee5c6bf29fa12d6cc0c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7794943135:AAG3U8Cu9U3I4EZOGu4WA_o-oToQSHNmYnU") 
+    API_ID = os.environ.get("API_ID", "23294809")
+    API_HASH = os.environ.get("API_HASH", "fbe5152195458e6aa8ec4298c96fb274")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7545122233:AAGlDCqGFJJRL75T8UYXERUQlMwzAgkBb3A") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://telegrambott19:telegrambott19@cluster0.hetez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sagnikgraviton847:AutoForward@autoforward.iwsux.mongodb.net/?retryWrites=true&w=majority&appName=AutoForward")
+    DB_NAME = os.environ.get("DB_NAME", "AutoForward")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '1226915127').split()]
 
 
 class temp(object): 
